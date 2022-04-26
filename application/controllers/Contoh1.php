3b79e9095723612079e4a0ba384b082c8cd2145c
mhmmdricho <muhammadricho75@gmail.com>
2022-04-26 17:04:58 +0700

    tugas wp pertemuan 7

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h>Perkenalkan</h1>";
+        echo" Nama saya Muhammad Richo
+              Saya tinggal di daerah Jakarta Pusat
+              Olahraga yang saya sukai adalah
+              Bermain Futsal";
+              
+                    
+
+    }
+}
